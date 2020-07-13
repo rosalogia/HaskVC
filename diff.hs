bbfdce11b27c6c98192bf6765dab854ef2cfc678
fshcat <jhoyos314@gmail.com>
2020-07-13 06:17:08 -0400

    Added diff file reassembly function

--- diff.hs
+++ diff.hs
@@ -1,10 +1,33 @@
 import System.Process
 import Text.Parsec
-
 rawDiff :: FilePath -> FilePath -> IO String
 rawDiff old new = do 
     (_,out,_) <- readProcessWithExitCode "diff" [old,new] ""
     return out
+
+assembleDiff :: Diff -> IO String
+assembleDiff diff = return $ concatStrings mapped
+    where
+    concatStrings (x:[]) = x
+    concatStrings (x:xs) = x ++ (concatStrings xs)
+
+    mapped = fmap concatBlock diff
+    concatBlock ((ln1,ind,ln2),changes)
+        | ind == 'd' = ln1 ++ [ind] ++ ln2 ++ "\n" ++ (handleDel changes) ++ "\n"
+        | ind == 'c' = ln1 ++ [ind] ++ ln2 ++ "\n" ++ (handleChanges changes "< ") ++ "\n"
+        | ind == 'a' = ln1 ++ [ind] ++ ln2 ++ "\n" ++ (handleAdd changes) ++ "\n"
+        | otherwise  = ""
+    
+    handleDel (x:[]) = "< " ++ x
+    handleDel (x:xs) = "< " ++ x ++ "\n" ++ (handleDel xs)  
+
+    handleAdd (x:[]) = "> " ++ x
+    handleAdd (x:xs) = "> " ++ x ++ "\n" ++ (handleAdd xs)
+
+    handleChanges (x:[]) prefix = prefix ++ x
+    handleChanges (x:xs) prefix = if x=="---" 
+                                  then (handleChanges xs "> ") 
+                                  else (prefix ++ x ++ "\n" ++ (handleChanges xs prefix))
 
 parseDiff :: IO String -> IO (Maybe Diff)
 parseDiff rawDiffIO = do 
@@ -55,3 +78,4 @@ parseChangeBlock = do
 pDiff :: Parsec String () Diff
 pDiff = many parseChangeBlock
     
+
