299df1841735a50b13e3f5ff63ec328b407e7875
fshcat <jhoyos314@gmail.com>
2020-07-21 20:11:56 -1000

    Added function to add entry to a tree, fixed errors

--- DirectoryTree.hs
+++ DirectoryTree.hs
@@ -1,7 +1,12 @@
-module DirectoryTree (directoryToTree, getInode) where
+module DirectoryTree (directoryToTree, addTreeEntry) where
 import System.Directory
 import System.Process
 import Data.Tree
+
+outputTree :: IO (Tree String) -> IO ()
+outputTree t = do
+    tree <- t
+    putStrLn . drawTree $ tree
 
 drawDirectoryTree :: FilePath -> IO ()
 drawDirectoryTree path = do
@@ -11,47 +16,36 @@ drawDirectoryTree path = do
 drawableDirectoryToTree :: FilePath -> IO (Tree String)
 drawableDirectoryToTree path = do
     unformated <- directoryToTree path
-    return $ formatter <$> unformated
-        where
-        formatter :: FileID -> String
-        formatter (int, st) = (show int) ++ ", " ++ st
-
--- Grabs inode number from a file
-getInode :: FilePath -> IO Integer
-getInode path = do
-    output <- readProcess "stat" ["-c","%i",path] ""
-    return . read $ output
+    return unformated
 
 listSubEntries :: FilePath -> IO [String]
 listSubEntries path = do
     isDirectory <- doesDirectoryExist path
     if isDirectory then listDirectory path else return []
 
-type FileID = (Integer, String)
+type FileID = String
 
 -- Turns directory into Tree FileID
 directoryToTree :: FilePath -> IO (Tree FileID)
 directoryToTree path = do
-    inode <- getInode path
     entryContents <- listSubEntries path
 
     let subTreesMonadic = map (subDirectoryToTree path) entryContents
     subTrees <- unwrapListMonads subTreesMonadic
 
-    return $ Node (inode, path) subTrees
+    return $ Node path subTrees
 
 subDirectoryToTree :: FilePath -> FilePath -> IO (Tree FileID)
 subDirectoryToTree pathAccum entry = do
     -- Grabs the contents of the directory as a list, then maps
     -- directoryToTree to it to turn the entries into trees
     let absPath = pathAccum ++ "/" ++ entry
-    inode <- getInode absPath
     entryContents <- listSubEntries absPath
 
     let subTreesMonadic = map (subDirectoryToTree absPath) entryContents
     subTrees <- unwrapListMonads subTreesMonadic
 
-    return $ Node (inode, entry) subTrees
+    return $ Node entry subTrees
 
 -- Turns list containing monad-wrapped values into
 -- a monad-wrapped list of values. Need this for directoryToTree,
@@ -64,51 +58,57 @@ unwrapListMonads (m:ms) = do
     return (entry:remaining)
 unwrapListMonads [] = return []
 
-pathToList :: FilePath -> [FilePath]
+pathToList :: FilePath -> [String]
 pathToList path = splitBySlash path "" []
     where
+    splitBySlash :: FilePath -> String -> [FilePath] -> [FilePath]
+    splitBySlash "" word list = list ++ [word]
     splitBySlash (char:rem) word list 
-        | char == '/' = splitBySlash rem "" (list ++ word)
-        | (char:rem) == "" = list
+        | char == '/' = splitBySlash rem "" (list ++ [word])
         | otherwise = splitBySlash rem (word ++ [char]) list
 
 listToPath :: [FilePath] -> FilePath
-listToPath pathList = foldl ("/"++) pathList
+listToPath pathList = foldl (++) "" $ map (\x -> "/" ++ x) pathList
 
 rmMatchHead :: Eq a => [a] -> [a] -> [a]
+rmMatchHead [] as2 = as2
 rmMatchHead (a1:as1) (a2:as2) 
-    | a1 == a2 = rmMatchHeaad as1 as2
+    | a1 == a2 = rmMatchHead as1 as2
     | otherwise = (a2:as2)
 
 getID :: FilePath -> FileID
-getID path = (getInode path, tail . pathToList $ path)
+getID path = last . pathToList $ path
 
 listToTree :: [FilePath] -> [FilePath] -> Tree FileID
 listToTree root (entry:[]) = Node entry []
-listToTree root (entry:rem) = Node (getID . listToPath $ root++entry) [listToTree (root ++ entry) rem]
+listToTree root (entry:rem) = 
+    Node (entry) [listToTree (root ++ [entry]) rem]
 
 extractMatchingName :: FilePath -> [Tree FileID] -> Maybe ([Tree FileID], Tree FileID)
 extractMatchingName target list = helper target [] list 
     where
     helper _ _ [] = Nothing
     helper tar passed (current:left) = 
-        let (Node (_,name) _) = current
+        let (Node name _) = current
         in if name == tar 
            then Just (passed++left, current)
            else helper tar (passed++[current]) left
-{-
-addTreeEntry :: FileID -> Tree FileID -> Tree FileID
-addTreeEntry p t = addEntry unrootedPath r t
-    where
-    Node (_,r) _ = t
-    unrootedPath = rmMatchHead (pathToList r) (pathToList p)
 
+addTreeEntry :: FileID -> Tree FileID -> Tree FileID
+addTreeEntry p t = addEntry unrootedPath (pathToList r) t
+    where
+    Node r _ = t
+    unrootedPath = rmMatchHead (pathToList r) (pathToList p)
+    
+    addEntry :: [FilePath] -> [FilePath] -> Tree FileID -> Tree FileID
     addEntry (entry:rem) passed tree =
-        let Node r treeList = tree
-            (_, parent) = r
-            extraction = extractMatchingName entry treeList
-        in if extraction == Nothing
-           then Node r (treeList ++ [listToTree passed (entry:rem)])
--}
+        let Node name treeList = tree
+            extracted = extractMatchingName entry treeList
+
+            processExtraction :: Maybe ([Tree FileID], Tree FileID) ->  Tree FileID
+            processExtraction Nothing = Node name $ (listToTree passed (entry:rem)):treeList
+            processExtraction (Just (filteredTrees, extraction)) = 
+                Node name $ (addEntry rem (passed ++ [entry]) extraction):filteredTrees 
+        in processExtraction extracted
 
 
